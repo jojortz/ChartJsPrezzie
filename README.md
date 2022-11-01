daa947cedd8b6c71682490f9b26705fea55e9730
Jojo Ortiz <ortiz.jroman@gmail.com>
2022-10-31 20:00:37 -0500

    initial setup of ChartJSPrezzie

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ChartJsPrezzie
+## Description
+Starter code for demonstrating abilities of Chart.js. Made for a presentation during the Senior Phase of Hack Reactor.
+
+## Sources
+[Chart.js Docs] (https://www.chartjs.org/docs/latest/)
+[react-chartjs-2](https://react-chartjs-2.js.org/examples)
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const axios = require('axios');
+require("dotenv").config();
+
+
+
+let port = 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
+
+app.listen(port, function () {
+  console.log(`listening on port ${port}`);
+});
